fa94e75f3ce2ea6fea334779bb01c0004ba6f45d
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-12-16 18:52:11 +1000

    first commit

---
+++ src/main.zig
@@ -0,0 +1,53 @@
+const std = @import("std");
+const net = std.net;
+
+pub fn handleConnection(conn: net.StreamServer.Connection, stdout: std.io.Writer) !void {
+    defer conn.stream.close();
+
+    var buf: [300]u8 = undefined;
+
+    const bytes = try conn.stream.read(&buf);
+    try stdout.print("[INFO] Received {d} bytes from client - {s}\n", .{ bytes, buf[0..bytes] });
+
+    _ = try conn.stream.write(
+        \\HTTP/1.1 200 OK
+        \\Content-Type: text/html; charset=UTF-8
+        \\Content-Length: 2000
+        \\
+        \\<!DOCTYPE html>
+        \\<html>
+        \\    <head>
+        \\        <title>Web server</title>
+        \\    </head>
+        \\    <body>
+        \\        <h1>My Zig web server</h1>
+        \\        <p>hello world!</p>
+        \\    </body>
+        \\</html>
+    );
+    // _ = try conn.stream.write("Hello from server!");
+}
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    var server = net.StreamServer.init(.{
+        .reuse_port = true,
+        .reuse_address = true,
+    });
+    defer {
+        server.close();
+        server.deinit();
+    }
+
+    const address = try net.Address.resolveIp("0.0.0.0", 3000);
+    try server.listen(address);
+    try stdout.print("[INFO] Server listening on {}\n", .{server.listen_address});
+
+    while (true) {
+        const conn = try server.accept();
+        errdefer conn.stream.close(); // If the thread fails to be created
+
+        _ = try std.Thread.spawn(.{}, handleConnection, .{ conn, stdout });
+    }
+}
